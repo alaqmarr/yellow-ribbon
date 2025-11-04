ac3a873e61c85e6becd8f9da6606a1303045add2
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-04 19:58:12 +0530

    deploy

--- src/data/information.js
+++ src/data/information.js
@@ -1,7 +1,7 @@
 const information = {
-  address: "88 Broklyn Street \n Road New York. USA",
-  phones: ["+92 666 888 0000", "666 888 0000"],
-  mails: ["needhelp@tevily.com", "info@tevily.com"],
+  address: "Hyderabad,\nTelangana,\nIndia.",
+  phones: ["+91 97899 05294", "+91 99850 95059"],
+  mails: ["yellowribbontravels@gmail.com", "info@yellowribbontravels.com"],
 };
 
 export default information;
