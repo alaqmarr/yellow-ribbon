2207e24c0c30546c59a72af6bcc00af04ee10ff7
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-07 00:35:28 +0530

    deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export async function getStaticProps() {
 
   return {
     props: { packages: packages.data, destinations: destinations.data },
-    revalidate: 60, // optional: ISR every 1 minute
+    revalidate: 120, // optional: ISR every 2 minutes
   };
 }
 export default Home;
