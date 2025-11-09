324117e987ce27daa48fc7e4b5de9734fd439fec
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-09 11:48:48 +0530

    deploy

--- src/components/NewsDetailsPage/NewsDetailsLeft.js
+++ src/components/NewsDetailsPage/NewsDetailsLeft.js
@@ -7,19 +7,23 @@ import CommentForm from "./CommentForm";
 import CommentOne from "./CommentOne";
 
 const NewsDetailsLeft = ({ data }) => {
-  const { thumbnail, title, author, images, html, categories } = data;
+  const { thumbnail, title, author, images, html, categories, createdAt } =
+    data;
   return (
     <div className="news-details__left">
       <div className="news-details__img">
         <Image src={thumbnail} alt="" />
         <div className="news-one__date">
           <p>
-            {date.split(" ").map((t, i) => (
-              <Fragment key={i}>
-                <span>{t}</span>
-                <br />
-              </Fragment>
-            ))}
+            {new Date(createdAt)
+              .toLocaleString("en-US", {})
+              .split(",")[0]
+              .split("/")
+              .map((t, i) => (
+                <Fragment key={i}>
+                  <span>{t}</span>
+                </Fragment>
+              ))}
           </p>
         </div>
       </div>
