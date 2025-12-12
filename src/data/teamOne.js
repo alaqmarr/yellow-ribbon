929f3a84878a376665ecc428dc33eabe96874638
AL AQMAR ⚡️ <112328620+alaqmarr@users.noreply.github.com>
2025-12-12 11:37:13 +0530

    Update teamOne.js

--- src/data/teamOne.js
+++ src/data/teamOne.js
@@ -27,28 +27,6 @@ const teamOne = {
         { id: 3, icon: "fab fa-instagram", href: "#" },
       ],
     },
-    {
-      id: 3,
-      image: "team-1-3.jpg",
-      name: "Sarah Bharmal",
-      title: "Marketing Head",
-      socials: [
-        { id: 1, icon: "fab fa-facebook", href: "#" },
-        { id: 2, icon: "fab fa-twitter", href: "#" },
-        { id: 3, icon: "fab fa-instagram", href: "#" },
-      ],
-    },
-    {
-      id: 4,
-      image: "team-1-4.jpg",
-      name: "Khadija",
-      title: "Bookings Head",
-      socials: [
-        { id: 1, icon: "fab fa-facebook", href: "#" },
-        { id: 2, icon: "fab fa-twitter", href: "#" },
-        { id: 3, icon: "fab fa-instagram", href: "#" },
-      ],
-    },
   ],
 };
 
